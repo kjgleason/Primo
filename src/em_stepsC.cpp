2fa1bf6c763f22c19862a7878bece380632f48f0
Kevin J. Gleason <kevin.j.gleason@gmail.com>
2018-07-26 15:34:56 -0500

    Test 1

--- src/em_stepsC.cpp
+++ src/em_stepsC.cpp
@@ -71,8 +71,7 @@ arma::rowvec findDiffGit(const arma::rowvec& old_pi, const arma::mat& Q, const a
   // calculate log density under each configuration
   arma::mat Bmat = log(D_0) * (1-t_Q) + log(D_1) * t_Q;
   // factor in proportion of observations estimated to belong to each configuration
-  log(old_pi);
-  return(log_pi);
+  return(log(old_pi));
   //Bmat.each_row() += log_pi;
 
   // substract minimum from each row (i.e. subtract colvec of rowMins from each column)
