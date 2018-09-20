5651a5d95c90f5812e8091ca8930897298418780
Kevin J. Gleason <kevin.j.gleason@gmail.com>
2018-09-20 14:13:05 -0500

    Use 'config_prop' estimated in main function when permuting (re-calculating 'config_prop' after permutation leads to poor eFDR calculation).

--- R/permute_func.R
+++ R/permute_func.R
@@ -8,6 +8,8 @@
 #' @param perm_col (vector of) numerical value(s); column number(s) to permute
 #' @param tol numerical value; specifies tolerance threshold for convergence.
 #' @param density_list list of densities estimated by \code{estimate_densities()}.
+#' @param config_prop vector of estimated proportion of SNPs
+#' belonging to each configuration
 #'
 #' @return A list with the following elements, based on permuted data:
 #' \tabular{ll}{
@@ -28,7 +30,7 @@
 #'
 #' @export
 #'
-permute_once_dens <- function(alt_props, perm_col, tol=1e-3, density_list){
+permute_once_dens <- function(alt_props, perm_col, tol=1e-3, density_list, config_prop){
   m <- nrow(density_list$D0)
   d <- ncol(density_list$D0)
 
@@ -40,10 +42,38 @@ permute_once_dens <- function(alt_props, perm_col, tol=1e-3, density_list){
     density_list$D1[,j] <- density_list$D1[od,j]
   }
 
-  # estimate configuration for permuted data
-  xx <- NULL
-  try({xx <- estimate_config(alt_props=alt_props, tol=tol, par_size=0, density_list=density_list)}, silent=TRUE)
-  return(xx)
+  # configuration matrix and number of patterns
+  Q <- make_qmat(1:d)
+  n_pattern <- nrow(Q)
+
+  # Re-calculate posterior probabilities under permutation
+  # maximum vector size in R is 2^31-1 (need different calculation if # elements in matrix exceeds)
+  if(m*as.double(n_pattern) <= 2^31-1){
+    curb<-e_step(config_prop, Q=Q, D0=density_list$D0, D1=density_list$D1)
+    # temporary solution to precision problems caused by |t-statistics| > 30
+    curb[which(is.na(curb))] <- 0
+    return(list(post_prob=curb,config_prop=config_prop,Tstat_mod=density_list$Tstat_mod,
+                D0=density_list$D0,D1=density_list$D1,alt_props=alt_props,tol=tol))
+  } else{
+    curb <- NULL
+    exit_e_step <-function(curpi,Q,D0,D1){
+      t_Q <- t(Q)
+      # consider using `bigmemory` package for following matrix
+      curb <- log(D0) %*% (1-t_Q) + log(D1) %*% t_Q
+      curb <- sweep(curb,2,log(curpi),"+")
+      # subtract minimum, then e^(B)/rowSums(B)
+      curb<-curb-matrixStats::rowMins(curb)
+      curb<-exp(curb)
+      # can use rowSums directly since columns are recycled
+      curb<-curb/matrixStats::rowSums2(curb)
+      # temporary solution to precision problems caused by |t-statistics| > 30
+      curb[which(is.na(curb))] <- 0
+      return(curb)
+    }
+    try(curb<-exit_e_step(curpi=config_prop,Q=Q,D0=density_list$D0,D1=density_list$D1))
+    return(list(post_prob=curb,config_prop=config_prop,Tstat_mod=density_list$Tstat_mod,
+                D0=density_list$D0,D1=density_list$D1,alt_props=alt_props,tol=tol))
+  }
 }
 
 
@@ -124,7 +154,9 @@ permute_once_stats <- function(betas, sds, mafs, dfs, alt_props, perm_col, tol=1
 #' (0 for sequential processing).
 #' @param tol numerical value; specifies tolerance threshold for convergence.
 #' @param perm_col numerical value; column number to permute
-#' @param density_list (optional) list of densities estimated by \code{estimate_densities()}.
+#' @param density_list list of densities estimated by \code{estimate_densities()}.
+#' @param config_prop vector of estimated proportion of SNPs
+#' belonging to each configuration
 #' @param perm_densities logical value; when true, permutes the previously calculated densities
 #' instead of reestimating densities from permuted betas and sds
 #'
@@ -150,11 +182,13 @@ permute_once_stats <- function(betas, sds, mafs, dfs, alt_props, perm_col, tol=1
 #'
 #' @export
 #'
-permute_once <- function(betas=NULL, sds=NULL, mafs=NULL, dfs=NULL, alt_props, perm_col, tol=1e-3, par_size=0, density_list=NULL, perm_densities=T){
+permute_once <- function(betas=NULL, sds=NULL, mafs=NULL, dfs=NULL, alt_props, perm_col, tol=1e-3, par_size=0,
+                         density_list=NULL, config_prop=NULL, perm_densities=T){
   if(perm_densities){
     if(is.null(density_list)) stop("Density list cannot be null if perm_densities=T")
-    return(permute_once_dens(alt_props=alt_props, perm_col=perm_col, tol=1e-3, density_list))
-  } else return(permute_once_stats(betas=betas, sds=sds, mafs=mafs, dfs=dfs, alt_props=alt_props, perm_col=perm_col, tol=tol, par_size=par_size))
+    return(permute_once_dens(alt_props=alt_props, perm_col=perm_col, tol=1e-3, density_list=density_list,config_prop=config_prop))
+  } else return(permute_once_stats(betas=betas, sds=sds, mafs=mafs, dfs=dfs, alt_props=alt_props,
+                                   perm_col=perm_col, tol=tol, par_size=par_size))
 }
 
 
@@ -177,6 +211,8 @@ permute_once <- function(betas=NULL, sds=NULL, mafs=NULL, dfs=NULL, alt_props, p
 #' @param perm_par_size numerical value; specifies the number of CPUs/cores/processors for
 #' parallel computing of permutations(0 for sequential processing).
 #' @param density_list (optional) list of densities estimated by \code{estimate_densities()}.
+#' @param config_prop vector of estimated proportion of SNPs
+#' belonging to each configuration
 #' @param perm_densities logical value; when true, permutes the previously calculated densities
 #' instead of reestimating densities from permuted betas and sds.
 #'
@@ -192,7 +228,8 @@ permute_once <- function(betas=NULL, sds=NULL, mafs=NULL, dfs=NULL, alt_props, p
 #'
 #' @export
 #'
-permute_integ <- function(betas=NULL, sds=NULL, mafs=NULL, dfs=NULL, alt_props, tol=1e-3, par_size=0, perm_par_size=0, density_list=NULL, perm_densities=T){
+permute_integ <- function(betas=NULL, sds=NULL, mafs=NULL, dfs=NULL, alt_props, tol=1e-3, par_size=0, perm_par_size=0,
+                          density_list=NULL, config_prop=NULL, perm_densities=T){
   # permute each column
   # parallel version
   if(perm_par_size>0){
@@ -203,14 +240,15 @@ permute_integ <- function(betas=NULL, sds=NULL, mafs=NULL, dfs=NULL, alt_props, 
                                function(j,betas,sds,mafs,dfs,alt_props,tol,par_size) {
                                  return(permute_once(betas=betas,sds=sds,mafs=mafs,
                                                      dfs=dfs, alt_props=alt_props,perm_col=j,
-                                                     tol=tol,par_size=par_size,density_list=density_list,perm_densities=perm_densities))
-                               },betas=betas,sds=sds,mafs=mafs,dfs=dfs,
-                               alt_props=alt_props,tol=tol,par_size=par_size,density_list=density_list,perm_densities=perm_densities)
+                                                     tol=tol,par_size=par_size,density_list=density_list,
+                                                     config_prop=config_prop,perm_densities=perm_densities))
+                               },betas=betas,sds=sds,mafs=mafs,dfs=dfs,alt_props=alt_props,tol=tol,
+                               par_size=par_size,density_list=density_list,config_prop=config_prop,perm_densities=perm_densities)
     parallel::stopCluster(clust)
   # sequential version
   # run estimate_config for each permuted dataset
   } else res <- lapply(1:ncol(betas), function(j)
-                        permute_once(betas,sds,mafs,dfs,alt_props,perm_col=j,tol,par_size,density_list,perm_densities))
+                        permute_once(betas,sds,mafs,dfs,alt_props,perm_col=j,tol,par_size,density_list,config_prop,perm_densities))
 
   return(res)
 }
@@ -252,11 +290,12 @@ permute_setup <- function(betas=NULL, sds=NULL, mafs=NULL, dfs=NULL, true_res, p
   # obtain parameters used with true data
   alt_props <- true_res$alt_props
   tol <- true_res$tol
+  config_prop <- true_res$config_prop
   if(is.null(density_list)){
     density_list <- list(Tstat_mod=true_res$Tstat_mod,D0=true_res$D0,D1=true_res$D0)
   }
   # run permutations using same parameters as true data
-  res <- permute_integ(betas, sds, mafs, dfs, alt_props, tol, par_size, perm_par_size, density_list, perm_densities=T)
+  res <- permute_integ(betas, sds, mafs, dfs, alt_props, tol, par_size, perm_par_size, density_list, config_prop, perm_densities=T)
   return(res)
 }
 
@@ -295,6 +334,7 @@ permute_multi <- function(betas=NULL, sds=NULL, mafs=NULL, dfs=NULL, true_res, n
   # obtain parameters used with true data
   alt_props <- true_res$alt_props
   tol <- true_res$tol
+  config_prop <- true_res$config_prop
   if(is.null(density_list)){
     density_list <- list(Tstat_mod=true_res$Tstat_mod,D0=true_res$D0,D1=true_res$D0)
   }
@@ -302,7 +342,7 @@ permute_multi <- function(betas=NULL, sds=NULL, mafs=NULL, dfs=NULL, true_res, n
   return_list <- list()
   for(p in 1:nperm){
     # run permutations using same parameters as true data
-    res <- permute_integ(betas, sds, mafs, dfs, alt_props, tol, par_size, perm_par_size, density_list, perm_densities)
+    res <- permute_integ(betas, sds, mafs, dfs, alt_props, tol, par_size, perm_par_size, density_list, config_prop, perm_densities)
     res <- lapply(res, function(x) x$post_prob)
     return_list[[p]] <- res
   }
