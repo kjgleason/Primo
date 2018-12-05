ec6227860e7d4231b16bcabe85b7118d38408f7e
Kevin J. Gleason <kevin.j.gleason@gmail.com>
2018-12-05 09:40:43 -0600

    Add manual page for package.

--- R/primo.R
+++ R/primo.R
@@ -1,3 +1,9 @@
+#' @details
+#' To estimate the posterior probabilities of belonging to each association pattern
+#' (i.e. configuration), use the \code{\link{estimate_config}} function.
+#'
+"_PACKAGE"
+
 #' @useDynLib primo, .registration=TRUE
 #' @importFrom Rcpp evalCpp
 #' @exportPattern "^[[:alpha:]]+"
