43af3753e55f9e4cb08edc8829fe8e1a1a929f16
Kevin J. Gleason <kevin.j.gleason@gmail.com>
2019-03-07 10:35:50 -0600

    Add grammatical consistency to documentation.

--- R/fdr.R
+++ R/fdr.R
@@ -3,11 +3,11 @@
 #' Calculate the empirical false discovery rate (FDR) given
 #' a vector of posterior probabilities and a specified threshold.
 #'
-#' @param post_prob A numeric vector of posterior probabilities.
-#' @param threshold Numerical value of the posterior probability threshold at which to
+#' @param post_prob numeric vector of posterior probabilities.
+#' @param threshold numeric value of the posterior probability threshold at which to
 #' calculate the empirical FDR.
 #'
-#' @return Numerical value of the empirical false discovery rate (FDR).
+#' @return A numeric value of the empirical false discovery rate (FDR).
 #'
 #' @export
 #'
@@ -23,16 +23,16 @@ calc_fdr <- function(post_prob,threshold){
 #' a vector of posterior probabilities and a specified threshold,
 #' adjusting for false positives identified by fine-mapping.
 #'
-#' @param post_prob A numeric vector of posterior probabilities.
-#' @param threshold Numerical value of the posterior probability threshold at which to
+#' @param post_prob numeric vector of posterior probabilities.
+#' @param threshold numerical value of the posterior probability threshold at which to
 #' calculate the empirical FDR.
-#' @param fail_idx An integer vector of the indices of observations which failed
+#' @param fail_idx integer vector of the indices of observations which failed
 #' fine-mapping. These are the observations where the association pattern with the
 #' highest posterior probability after fine-mapping changed (if using collapsed
 #' categories, they should have changed such they are no longer in an
 #' association pattern fitting the description of the collapsed category).
 #'
-#' @return Numerical value of the empirical false discovery rate (FDR).
+#' @return A numeric value of the empirical false discovery rate (FDR).
 #'
 #' @export
 #'
@@ -53,13 +53,13 @@ calc_fdr_finemap <- function(post_prob,threshold,fail_idx){
 #' Calculate the empirical false discovery rate (FDR) of a
 #' column in a matrix of posterior probabilities given a specified threshold.
 #'
-#' @param post_prob A numeric matrix of posterior probabilities.
-#' @param threshold Numerical value of the posterior probability threshold at which to
+#' @param post_prob numeric matrix of posterior probabilities.
+#' @param threshold numeric value of the posterior probability threshold at which to
 #' calculate the empirical FDR.
-#' @param col_name Character string of the column name (optional).
-#' @param col_idx Integer of the column index (optional).
+#' @param col_name character string of the column name (optional).
+#' @param col_idx integer of the column index (optional).
 #'
-#' @return Numerical value of the empirical false discovery rate (FDR).
+#' @return A numeric value of the empirical false discovery rate (FDR).
 #'
 #' @details At least one of \code{col_name} or \code{col_idx} must be specified.
 #'
@@ -87,13 +87,13 @@ calc_fdr_col <- function(post_prob,threshold,col_name=NULL,col_idx=NULL){
 #' Calculate the empirical false discovery rate (FDR) of specified
 #' columns in a matrix of posterior probabilities given a specified threshold.
 #'
-#' @param post_prob A numeric matrix of posterior probabilities.
-#' @param threshold Numerical value of the posterior probability threshold at which to
+#' @param post_prob numeric matrix of posterior probabilities.
+#' @param threshold numeric value of the posterior probability threshold at which to
 #' calculate the empirical FDR.
-#' @param col_names A character vector of column names (optional).
-#' @param col_idxs An integer vector of column indices (optional).
+#' @param col_names character vector of column names (optional).
+#' @param col_idxs integer vector of column indices (optional).
 #'
-#' @return Numerical value of the empirical false discovery rate (FDR).
+#' @return A numeric value of the empirical false discovery rate (FDR).
 #'
 #' @details At least one of \code{col_names} or \code{col_idxs} must be specified.
 #'
