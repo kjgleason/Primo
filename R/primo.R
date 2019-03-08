23468edcf884072bfca7a7990516e2bef02a5946
Kevin J. Gleason <kevin.j.gleason@gmail.com>
2019-03-08 16:26:18 -0600

    Update package details with new function names.

--- R/primo.R
+++ R/primo.R
@@ -1,6 +1,7 @@
 #' @details
-#' To estimate the posterior probabilities of belonging to each association pattern
-#' (i.e. configuration), use the \code{\link{estimate_config}} function.
+#' To estimate the posterior probabilities of belonging to each association pattern,
+#' either user the wrapper function \code{\link{Primo}}, or directly call
+#' \code{\link{Primo_tstat}} (for t-statistics) or \code{\link{Primo_pval}} (for p-values).
 #'
 "_PACKAGE"
 
