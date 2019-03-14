7d35291b161bbd84614e14fcf550059711ea5978
Kevin J. Gleason <kevin.j.gleason@gmail.com>
2019-03-14 13:14:05 -0500

    Add function to estimate posterior probabilities for observations missing one or more data types.

--- R/processing.R
+++ R/processing.R
@@ -133,3 +133,111 @@ find_leadSNPs <- function(data,SNP_col,pheno_cols,stat_cols,data_type="pvalue",s
 
   return(leadSNPs_byRegion)
 }
+
+#' Estimate posterior probabilities for observations missing from original Primo analysis.
+#'
+#' @export
+#'
+Primo_missData_tstat <- function(betas,sds,dfs,trait_idx,mafs=NULL,pis,Gamma,prior_df,prior_var,unscaled_var,par_size=1){
+  m <- nrow(betas)
+  d <- ncol(betas)
+  # orig_d <- log(length(pis),2)
+  orig_d <- log(ncol(pis),2)
+
+  miss_idx <- (1:orig_d)[-trait_idx]
+
+  ## extract data for traits not missing data
+  d0 <- prior_df[trait_idx]
+  s02 <- prior_var[trait_idx]
+  v0 <- unscaled_var[trait_idx]
+  Gamma <- Gamma[trait_idx,trait_idx]
+
+  orig_Q <- primo::make_qmat(1:orig_d)
+  if(length(miss_idx) == 1){
+    keep_patterns <- which(orig_Q[,miss_idx]==0)
+  } else{
+    keep_patterns <- which(rowSums(orig_Q[,miss_idx])==0)
+  }
+  # pis <- pis[keep_patterns]
+  pis <- pis[,keep_patterns]
+
+
+  ## estimate marginal density functions in limma framework
+  density_list <- lapply(1:d, function(j){
+    if(is.matrix(mafs)) mafs <- mafs[,j]
+    if(is.matrix(dfs)) {
+      d1 <- dfs[,j]
+    } else d1 <- rep(dfs[j],m)
+
+    ## account for MAF in variance calculations
+    if (is.null(mafs)){
+      v1 = rep(1,m)
+      sigma2 <- sds[,j]^2
+    } else{
+      v1 <- 1/(2*mafs*(1-mafs))
+      sigma2 <- sds[,j]^2*(2*mafs*(1-mafs))
+    }
+
+    ## rescale t-statistic (see Smyth, 2004)
+    sg_tilde <- sqrt((d0[j]*s02[j]+d1*sigma2)/(d0[j]+d1))
+    moderate.t <- betas[,j]/(sg_tilde*sqrt(v1))
+
+    ## estimate null and alternative densities
+    df_mod=d0[j]+d1
+    scaler=sqrt(1+v0[j]/v1)
+
+    return(list(Tstat_mod = moderate.t, df_mod=df_mod, scaler=scaler, prior_df=d0[j], prior_var=s02[j], unscaled_var=v0[j]))
+  } )
+
+  ## extract parameters for pattern-specific density estimation
+  Tstat_mod <- do.call("cbind", lapply(density_list, function(x) x$Tstat_mod))
+  mdfs <- do.call("cbind", lapply(density_list, function(x) x$df_mod))
+  V <- do.call("cbind", lapply(density_list, function(x) x$scaler))
+  ## extract parameters from marginal densities (can be used to estimate variables for new observations)
+  prior_df <- sapply(density_list, function(x) x$prior_df)
+  prior_var <- sapply(density_list, function(x) x$prior_var)
+  unscaled_var <- sapply(density_list, function(x) x$unscaled_var)
+
+  ## create sd matrix from moderated t-statistic dfs
+  mdf_sd_mat <- sqrt(mdfs/(mdfs-2))
+
+  ## computation of D_mat (densities under each pattern)
+  Q<-make_qmat(1:d)
+  D_mat_func <- function(k){
+    m=nrow(V)
+    d=ncol(V)
+    v_k <-   V %*%diag(Q[k,]) + matrix(1, nrow=nrow(V),ncol=ncol(V))%*%diag( 1-Q[k,])
+    v_k <- v_k * mdf_sd_mat
+    Z <- Tstat_mod/v_k
+    dets <- exp(rowSums(log(v_k)))
+    return(mvtnorm::dmvnorm(Z, mean=rep(0,d),sigma=Gamma)/dets)
+  }
+
+  ## parallel version
+  if(par_size > 1){
+    cl <- parallel::makeCluster(par_size)
+    parallel::clusterEvalQ(cl, library(mvtnorm))
+    parallel::clusterExport(cl=cl, varlist=list("D_mat_func","Tstat_mod","V","mdf_sd_mat","Q","Gamma"),envir=environment())
+    D_mat_byk <- parallel::parLapply(cl, 1:2^d, function(k) D_mat_func(k=k))
+    parallel::stopCluster(cl)
+  } else{
+    ## sequential version
+    D_mat_byk <- lapply(1:2^d, function(k) D_mat_func(k=k))
+  }
+
+  ## bind densities together into matrix
+  D_mat <- do.call("cbind",D_mat_byk)
+  rm(D_mat_byk)
+
+
+  ## obtain posterior probabilities
+  PP <- sweep(log(D_mat),2,log(pis),"+")
+  # subtract maximum, then e^(B)/rowSums(B)
+  PP<-PP-matrixStats::rowMaxs(PP)
+  PP<-exp(PP)
+  # use rowSums directly since columns are recycled
+  PP<-PP/rowSums(PP)
+
+  return(list(post_prob=PP, pis=pis, D_mat=D_mat, Gamma=Gamma, Tstat_mod=Tstat_mod, V_mat = V, mdf_sd_mat = mdf_sd_mat,
+              prior_df=prior_df,prior_var=prior_var,unscaled_var=unscaled_var))
+}
